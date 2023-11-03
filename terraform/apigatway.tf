8e4c938e3795a07fe26d111d17cecbcf5ec98219
MunaizAhmed <munaiz@munaizahmed.com>
2023-11-02 21:39:24 -0500

    configured APIGateway lambda integration for root resource using terraform. branch name says vpc_rds but I needed to set the apigateway configurations right before moving on.

--- terraform/apigatway.tf
+++ terraform/apigatway.tf
@@ -1,36 +1,32 @@
 resource "aws_api_gateway_rest_api" "main_lambda_api" {
   name        = "main_lambda_api"
   description = "API for triggering main_lambda"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
-resource "aws_api_gateway_resource" "example_resource" {
-  rest_api_id = aws_api_gateway_rest_api.main_lambda_api.id
-  parent_id   = aws_api_gateway_rest_api.main_lambda_api.root_resource_id
-  path_part   = "trigger"
-}
-
-resource "aws_api_gateway_method" "example_method" {
+resource "aws_api_gateway_method" "root_get" {
   rest_api_id   = aws_api_gateway_rest_api.main_lambda_api.id
-  resource_id   = aws_api_gateway_resource.example_resource.id
+  resource_id   = aws_api_gateway_rest_api.main_lambda_api.root_resource_id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "main_lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.main_lambda_api.id
-  resource_id = aws_api_gateway_resource.example_resource.id
-  http_method = aws_api_gateway_method.example_method.http_method
-
-  type      = "AWS_PROXY"
-  uri       = aws_lambda_function.main_lambda.invoke_arn
+  rest_api_id             = aws_api_gateway_rest_api.main_lambda_api.id
+  resource_id             = aws_api_gateway_rest_api.main_lambda_api.root_resource_id
+  http_method             = aws_api_gateway_method.root_get.http_method
   integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "example_deployment" {
+resource "aws_api_gateway_deployment" "dev_deployment" {
   depends_on = [aws_api_gateway_integration.main_lambda_integration]
 
   rest_api_id = aws_api_gateway_rest_api.main_lambda_api.id
-  stage_name  = "prod"
+  stage_name  = "dev"
 }
 
 resource "aws_lambda_permission" "apigw_lambda_permission" {
