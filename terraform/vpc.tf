47d3f11dfaeeacc621f37f884734062118e23285
MunaizAhmed <munaiz@munaizahmed.com>
2023-11-04 10:00:01 -0500

    rearranged code in vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -22,24 +22,24 @@ resource "aws_subnet" "public_subnet_a" {
   }
 }
 
-resource "aws_subnet" "public_subnet_b" {
+resource "aws_subnet" "private_subnet_a" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "private_subnet_a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "public_subnet_b"
-  }
-}
-
-resource "aws_subnet" "private_subnet_a" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    Name = "private_subnet_a"
   }
 }
 
