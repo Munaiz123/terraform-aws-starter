0e12d4208414bc5fc8fcf2851e1ddfc00a07f19b
MunaizAhmed <munaiz@munaizahmed.com>
2023-11-04 09:39:37 -0500

    added VPC bolierplate - configured public/public subnets in 2 AZs, internetgateways, routetables, routetable associations and subnet group (needs atleast 2 AZs)

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,30 +1,74 @@
-resource "aws_vpc" "main-vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "main-vpc"
+    Name = "main_vpc"
   }
 }
 
-resource "aws_subnet" "subnet-a" {
-  vpc_id                  = aws_vpc.main-vpc.id
+resource "aws_internet_gateway" "main_vpc_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-a"
+    Name = "public_subnet_a"
   }
 }
 
-resource "aws_subnet" "subnet-b" {
-  vpc_id                  = aws_vpc.main-vpc.id
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-b"
+    Name = "public_subnet_b"
   }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "private_subnet_a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "private_subnet_b"
+  }
+}
+
+resource "aws_route_table" "main_vpc_public_routetable" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_vpc_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.main_vpc_public_routetable.id  
+}
+
+resource "aws_db_subnet_group" "main_vpc_subnet_group" {
+  name = "main_vpc_subnet_group"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+
 }
