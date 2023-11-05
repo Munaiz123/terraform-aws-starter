e7bfc5b42c2f28359bfef2ef5986cd9d5740e7ec
MunaizAhmed <munaiz@munaizahmed.com>
2023-11-04 21:30:36 -0500

    updating .zip name in S3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "main_lambda_bucket" {
 resource "aws_s3_object" "main" {
   bucket = aws_s3_bucket.main_lambda_bucket.bucket
 
-  key    = "archive.zip"
+  key    = "src.zip"
   source = data.archive_file.main_lambda_archive.output_path
 
   etag = filemd5(data.archive_file.main_lambda_archive.output_path)
